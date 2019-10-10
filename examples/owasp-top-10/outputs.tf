c20ab1073b4d2a81186a75f8db75793570af0cd6
Szymon Wolszczak <szymon.wolszczak@tsh.io>
2019-10-10 12:14:40 +0200

    updated to terraform0.12.10

--- examples/owasp-top-10/outputs.tf
+++ examples/owasp-top-10/outputs.tf
@@ -3,42 +3,42 @@ output "rule_group_id" {
   value       = "${module.owasp_top_10.rule_group_id}"
 }
 
-output "rule01_sql_injection_rule_id" {
+output "rule_sql_injection_rule_id_01" {
   description = "AWS WAF Rule which mitigates SQL Injection Attacks."
-  value       = "${module.owasp_top_10.rule01_sql_injection_rule_id}"
+  value       = "${module.owasp_top_10.rule_sql_injection_rule_id_01}"
 }
 
-output "rule02_auth_token_rule_id" {
+output "rule_auth_token_rule_id_02" {
   description = "AWS WAF Rule which blacklists bad/hijacked JWT tokens or session IDs."
-  value       = "${module.owasp_top_10.rule02_auth_token_rule_id}"
+  value       = "${module.owasp_top_10.rule_auth_token_rule_id_02}"
 }
 
-output "rule03_xss_rule_id" {
+output "rule_xss_rule_id_03" {
   description = "AWS WAF Rule which mitigates Cross Site Scripting Attacks."
-  value       = "${module.owasp_top_10.rule03_xss_rule_id}"
+  value       = "${module.owasp_top_10.rule_xss_rule_id_03}"
 }
 
-output "rule04_paths_rule_id" {
+output "rule_paths_rule_id_04" {
   description = "AWS WAF Rule which mitigates Path Traversal, LFI, RFI."
-  value       = "${module.owasp_top_10.rule04_paths_rule_id}"
+  value       = "${module.owasp_top_10.rule_paths_rule_id_04}"
 }
 
-output "rule06_php_insecure_rule_id" {
+output "rule_php_insecure_rule_id_06" {
   description = "AWS WAF Rule which mitigates PHP Specific Security Misconfigurations."
-  value       = "${module.owasp_top_10.rule06_php_insecure_rule_id}"
+  value       = "${module.owasp_top_10.rule_php_insecure_rule_id_06}"
 }
 
-output "rule07_size_restriction_rule_id" {
+output "rule_size_restriction_rule_id_07" {
   description = "AWS WAF Rule which mitigates abnormal requests via size restrictions."
-  value       = "${module.owasp_top_10.rule07_size_restriction_rule_id}"
+  value       = "${module.owasp_top_10.rule_size_restriction_rule_id_07}"
 }
 
-output "rule08_csrf_rule_id" {
+output "rule_csrf_rule_id_08" {
   description = "AWS WAF Rule which enforces the presence of CSRF token in request header."
-  value       = "${module.owasp_top_10.rule08_csrf_rule_id}"
+  value       = "${module.owasp_top_10.rule_csrf_rule_id_08}"
 }
 
-output "rule09_server_side_include_rule_id" {
+output "rule_server_side_include_rule_id_09" {
   description = "AWS WAF Rule which blocks request patterns for webroot objects that shouldn't be directly accessible."
-  value       = "${module.owasp_top_10.rule09_server_side_include_rule_id}"
+  value       = "${module.owasp_top_10.rule_server_side_include_rule_id_09}"
 }
